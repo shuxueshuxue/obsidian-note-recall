ef89874d3df3f71629863fffd4f58d18148788e0
shuxueshuxue <shuxue@protonmail.coom>
2023-03-04 18:02:28 +0800

    fixed some minor bugs

--- main.ts
+++ main.ts
@@ -3,11 +3,11 @@ import * as path from 'path';
 import { getRandomIndexes, similarity } from 'tools';
 
 interface MyPluginSettings {
-	mySetting: string;
+	difficuty: number;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	difficuty: 10  
 }
 
 class SavedData {
@@ -34,35 +34,22 @@ export default class NoteRecall extends Plugin {
 		}
 	}
 
-	async create_file(content: string){
-		this.app.vault.create("challenge.md", content)
-		this.app.workspace.openLinkText("challenge.md", "/");
-
-		// await this.saveData(new SavedData(['1', '2']))
-		// let loaded_data = await this.loadData()
-		// console.log(loaded_data.words)
-	}
-
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		this.addCommand({
 			id: 'start-game',
 			name: 'Start game',
 			callback: async () => {
-				let modified_article = await this.get_file_content()
+
+				const activeFile = app.workspace.getActiveFile();
+
+				if (activeFile == null){
+					new Notice("Please open a note file!")
+					return
+				}
+
+				let modified_article = await app.vault.cachedRead(activeFile)
 				
 				const englishWords: string[] = [];
 				const wordPositions: number[] = [];
@@ -78,23 +65,19 @@ export default class NoteRecall extends Plugin {
 
 				const number_range = [...Array(englishWords.length).keys()];
 
-				const chosenIndexes = getRandomIndexes(number_range, Math.round(englishWords.length / 10));
+				const chosenIndexes = getRandomIndexes(number_range, Math.round(englishWords.length / this.settings.difficuty));
 				chosenIndexes.sort((a, b) => b - a);
 
 				const chosenWords = chosenIndexes.map(index => englishWords[index]);
 				
-				const activeFile = app.workspace.getActiveFile();
+				
 
 				// save it to data.json
 
 				if (activeFile){
 					this.saveData(new SavedData(chosenWords.slice().reverse(), activeFile.path))
 				}
-				console.log(chosenWords.slice().reverse(), chosenWords)
 				
-
-				// console.log(englishWords, chosenIndexes, chosenWords)
-				// console.log(chosenIndexes, chosenWords)
 				for (const index of chosenIndexes) {
 					modified_article = modified_article.slice(0, wordPositions[index]) + "🏴🏴" + modified_article.slice(wordPositions[index] + englishWords[index].length);
 				}
@@ -174,74 +157,10 @@ export default class NoteRecall extends Plugin {
 
 			}
 		})
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: NoteRecall;
-
-	constructor(app: App, plugin: NoteRecall) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
